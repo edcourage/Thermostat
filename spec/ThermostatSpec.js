22e8bb4ccd24a8eea37dc03ee81e32aef926dfb1
Ed Courage <edcourage@live.com>
2019-11-13 15:52:45 +0000

    you can now turn temperature down - temperature cannot go below minimum temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,9 +1,17 @@
 describe("Thermostat", function() {
 
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat
+  })
+
+
+
   describe("#new instance", function() {
 
     it("thermostats default is 20", function() {
-      var thermostat = new Thermostat;
+
       expect(thermostat.currentTemp()).toEqual(20);
 
     });
@@ -12,28 +20,54 @@ describe("Thermostat", function() {
   describe("#upTemp", function() {
 
     it("turns temperature up by 2 as default", function() {
-      var thermostat = new Thermostat;
+
       thermostat.upTemp()
       expect(thermostat.currentTemp()).toEqual(22);
     });
 
     it("can override default temp change", function() {
-      var thermostat = new Thermostat;
+
       thermostat.upTemp(4)
       expect(thermostat.currentTemp()).toEqual(24);
     });
 
     it("if temperature will never go above maximum and error with be thrown", function() {
-      var thermostat = new Thermostat;
+
 
       expect(function(){thermostat.upTemp(100)}).toThrow("Max Temperature Reached");
       expect(thermostat.currentTemp()).toEqual(25);
-
-
-
     })
 
+
+
   });
+
+  describe("#downTemp", function() {
+
+    it("turns temperature down by 2 as default", function() {
+
+      thermostat.downTemp()
+      expect(thermostat.currentTemp()).toEqual(18);
+    });
+
+    it("can override default temp change", function() {
+
+      thermostat.downTemp(4)
+      expect(thermostat.currentTemp()).toEqual(16);
+    });
+
+    it("if temperature will never go below minimum and error with be thrown", function() {
+
+
+      expect(function(){thermostat.downTemp(100)}).toThrow("Max Temperature Reached");
+      expect(thermostat.currentTemp()).toEqual(10);
+    })
+
+
+
+  });
+
+
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,7 @@
 function Thermostat() {
   this._temp = 20
-  this.maxTemp = 25
+  this._maxTemp = 25
+  this._minTemp = 10
 }
 
 Thermostat.prototype.currentTemp = function() {
@@ -8,12 +9,21 @@ Thermostat.prototype.currentTemp = function() {
 }
 
 Thermostat.prototype.upTemp = function(temp = 2) {
-  if ((this._temp + temp) > this.maxTemp) {
-    this._temp = this.maxTemp
+  if ((this._temp + temp) > this._maxTemp) {
+    this._temp = this._maxTemp
     throw "Max Temperature Reached"
   }
   else {
     this._temp += temp
   }
+}
 
+Thermostat.prototype.downTemp = function(temp = 2) {
+  if ((this._temp - temp) < this._minTemp) {
+    this._temp = this._minTemp
+    throw "Max Temperature Reached"
+  }
+  else {
+    this._temp -= temp
+  }
 }
