a77b16a78342f25fed4a28176e55c7eb4ff0111a
Ed Courage <edcourage@live.com>
2019-11-13 15:42:58 +0000

    Temperture can no longer be put over max temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,25 +3,35 @@ describe("Thermostat", function() {
   describe("#new instance", function() {
 
     it("thermostats default is 20", function() {
-    var thermostat = new Thermostat;
-    expect(thermostat.currentTemp()).toEqual(20);
+      var thermostat = new Thermostat;
+      expect(thermostat.currentTemp()).toEqual(20);
 
     });
   });
 
   describe("#upTemp", function() {
 
-    it("turns temperature up by 5 as default", function() {
-    var thermostat = new Thermostat;
-    thermostat.upTemp()
-    expect(thermostat.currentTemp()).toEqual(25);
+    it("turns temperature up by 2 as default", function() {
+      var thermostat = new Thermostat;
+      thermostat.upTemp()
+      expect(thermostat.currentTemp()).toEqual(22);
     });
 
-    it("can override defult temp change", function() {
-    var thermostat = new Thermostat;
-    thermostat.upTemp(13)
-    expect(thermostat.currentTemp()).toEqual(33);
+    it("can override default temp change", function() {
+      var thermostat = new Thermostat;
+      thermostat.upTemp(4)
+      expect(thermostat.currentTemp()).toEqual(24);
     });
+
+    it("if temperature will never go above maximum and error with be thrown", function() {
+      var thermostat = new Thermostat;
+
+      expect(function(){thermostat.upTemp(100)}).toThrow("Max Temperature Reached");
+      expect(thermostat.currentTemp()).toEqual(25);
+
+
+
+    })
 
   });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,19 @@
 function Thermostat() {
   this._temp = 20
+  this.maxTemp = 25
 }
 
 Thermostat.prototype.currentTemp = function() {
   return this._temp
 }
 
-Thermostat.prototype.upTemp = function(temp = 5) {
-  this._temp += temp
+Thermostat.prototype.upTemp = function(temp = 2) {
+  if ((this._temp + temp) > this.maxTemp) {
+    this._temp = this.maxTemp
+    throw "Max Temperature Reached"
+  }
+  else {
+    this._temp += temp
+  }
 
 }
